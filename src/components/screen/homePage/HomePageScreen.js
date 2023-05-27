0ec7ad588a3b180154395ea728e9d206e7e9e336
Rupesh <0c4ab1534e091e7812507ac06286f5df@protonmail.com>
2023-05-27 14:23:05 +1000

    minor changes

--- src/components/screen/homePage/HomePageScreen.js
+++ src/components/screen/homePage/HomePageScreen.js
@@ -5,7 +5,8 @@ export default function HomePageScreen() {
   return (
     <div>
           <h1>This is the home page</h1>
-            <h1>Welcome to Synergic</h1>
+          <h1>Welcome to Synergic</h1>
+          <h1>This is the home page</h1>
     </div>
   )
 }
